1b279c91b0e435808f419c4c69d8777e8c74351f
Yuan-Hsi <tobby382@gmail.com>
2025-02-12 14:08:31 +0900

    security fix. prevent NoSQL injection and XSS

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
 const postRouter = require("./Routes/postRoutes");
 const userRouter = require("./Routes/userRoutes");
 const appError = require("./Function/utils/appError");
@@ -8,8 +14,28 @@ const errorHandler = require("./Function/errorFunction");
 const app = express();
 
 // middleware
+app.use(helmet()); // set security HTTP headers
 app.use(morgan("dev"));
 app.use(express.json());
+const limiter = rateLimit({
+  max: 300, // max require amount for same IP
+  windowMs: 60 * 60 * 1000, // for how long
+  message: "To many requests from the IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// prevent the NoSQL injection
+app.use(mongoSanitize());
+
+// prevent XSS attack
+app.use(xss());
+
+// prevent parameter pollution, like {url}/sort=duration&sort=price => it will become an array, which might be a threat
+app.use(
+  hpp({
+    whitelist: ["timestamp"],
+  })
+);
 
 app.use("/api/v1/post", postRouter);
 app.use("/api/v1/user", userRouter);
