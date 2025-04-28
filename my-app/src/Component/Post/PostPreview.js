581d3fe0d920dfce5e8756f749be6ac09f584013
Yuan-Hsi <tobby382@gmail.com>
2025-04-28 10:56:00 +0900

    post loading function complete

--- my-app/src/Component/Post/PostPreview.js
+++ my-app/src/Component/Post/PostPreview.js
@@ -23,10 +23,18 @@ const verticalMoreStyle = {
   paddingBottom: "3.5%",
 };
 
+const loadingStyle = {
+  position: "relative",
+  overflow: "hidden",
+  backgroundColor: "#f2c700",
+  borderRadius: "5px",
+};
+
 function PostPreview(props) {
   const [posts, setPosts] = useState([1, 2, 3, 4]);
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(1);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { user } = useUser();
 
@@ -53,7 +61,7 @@ function PostPreview(props) {
       if (data.status === "success") {
         setTotalPage(Math.ceil(data.totalResults / 4));
         setPosts(data.data.posts);
-        console.log(data.data.posts);
+        setLoading(false);
       }
     }
 
@@ -97,75 +105,90 @@ function PostPreview(props) {
 
   return (
     <div className='preSection' style={{ margin: props.margin, width: "130%" }}>
+      {loading && (
+        <div className='prePost loading' style={isVertical ? verticalPostStyle : {}}>
+          <div className='prePhoto loadingImg' style={{ height: "70%", width: mySize.adjust(0.3), ...loadingStyle }}></div>
+          <div style={{ width: "100%" }}>
+            <div className='preTitleSection loadingTitleSec' style={{ justifyContent: "flex-start" }}>
+              <div className='preTitle loadingTitle' style={{ width: "60%", height: mySize.adjust(isVertical ? 0.03 : 0.04), marginTop: "0", ...loadingStyle }}></div>
+              <div className='forWho loadingWho' style={isVertical ? verticalForWhoStyle : {}}></div>
+            </div>
+            <div className='preContext loadingContext' style={{ width: "80%", height: mySize.adjust(isVertical ? 0.05 : 0.055), marginTop: isVertical ? "5%" : "1.5%", ...loadingStyle }}></div>
+            <div className='preContext loadingContext' style={{ width: "80%", height: mySize.adjust(isVertical ? 0.05 : 0.055), marginTop: isVertical ? "3%" : "1.5%", ...loadingStyle }}></div>
+          </div>
+        </div>
+      )}
+
       {/* Post list */}
-      {posts.map((post, idx) => {
-        let postContent = "";
-        let firstImageBase64 = null;
+      {!loading &&
+        posts.map((post, idx) => {
+          let postContent = "";
+          let firstImageBase64 = null;
 
-        if (post.content !== undefined) {
-          // Decode HTML entities
-          const htmlContent = he.decode(post.content);
+          if (post.content !== undefined) {
+            // Decode HTML entities
+            const htmlContent = he.decode(post.content);
 
-          // Drop the decoration of the content
-          postContent = htmlContent.replace(/<[^>]+>/g, "");
+            // Drop the decoration of the content
+            postContent = htmlContent.replace(/<[^>]+>/g, "");
 
-          // Read the imgfile (only first one)
-          const imgRegex = /<img[^>]+src="([^">]+)"/g;
-          const match = imgRegex.exec(htmlContent);
-          firstImageBase64 = match ? match[1] : null;
-        }
+            // Read the imgfile (only first one)
+            const imgRegex = /<img[^>]+src="([^">]+)"/g;
+            const match = imgRegex.exec(htmlContent);
+            firstImageBase64 = match ? match[1] : null;
+          }
 
-        return (
-          <div className='prePost' key={`prePost${idx}`} style={isVertical ? verticalPostStyle : {}}>
-            {/* Delete button - only visible for admin or post author */}
-            {(user.title === "admin" || (post.author && user.id === post.author[0]._id)) && (
-              <button className='delBtn' onClick={() => delPost(post.id)}>
-                ✖
-              </button>
-            )}
+          return (
+            <div className='prePost' key={`prePost${idx}`} style={isVertical ? verticalPostStyle : {}}>
+              {/* Delete button - only visible for admin or post author */}
+              {(user.title === "admin" || (post.author && user.id === post.author[0]._id)) && (
+                <button className='delBtn' onClick={() => delPost(post.id)}>
+                  ✖
+                </button>
+              )}
 
-            {/* Post thumbnail image */}
-            <img className='prePhoto' src={firstImageBase64} alt=''></img>
+              {/* Post thumbnail image */}
+              <img className='prePhoto' src={firstImageBase64} alt=''></img>
 
-            <div style={{ width: "100%" }}>
-              {/* Post title section */}
-              <div className='preTitleSection'>
-                <h1
-                  className='preTitle'
-                  title={post.title}
-                  style={{ fontSize: mySize.adjust(isVertical ? 0.03 : 0.04), overflow: "hidden", textOverflow: "ellipsis", display: "-webkit-box", WebkitBoxOrient: "vertical", WebkitLineClamp: 2 }}
+              <div style={{ width: "100%" }}>
+                {/* Post title section */}
+                <div className='preTitleSection'>
+                  <h1
+                    className='preTitle'
+                    title={post.title}
+                    style={{ fontSize: mySize.adjust(isVertical ? 0.03 : 0.04), overflow: "hidden", textOverflow: "ellipsis", display: "-webkit-box", WebkitBoxOrient: "vertical", WebkitLineClamp: 2 }}
+                  >
+                    {post.title}
+                  </h1>
+
+                  {/* Target audience indicator */}
+                  <div className='forWho' style={isVertical ? verticalForWhoStyle : {}}>
+                    <p style={{ fontSize: mySize.adjust(0.018), letterSpacing: "0.1em", overflow: "hidden", textOverflow: "clip", whiteSpace: "nowrap" }}>{forwho(post)}</p>
+                  </div>
+                </div>
+
+                {/* Post content preview - limited to 3 lines */}
+                <div
+                  className='preContext'
+                  style={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitBoxOrient: "vertical",
+                    WebkitLineClamp: 3,
+                  }}
                 >
-                  {post.title}
-                </h1>
-
-                {/* Target audience indicator */}
-                <div className='forWho' style={isVertical ? verticalForWhoStyle : {}}>
-                  <p style={{ fontSize: mySize.adjust(0.018), letterSpacing: "0.1em", overflow: "hidden", textOverflow: "clip", whiteSpace: "nowrap" }}>{forwho(post)}</p>
+                  <p style={{ fontSize: mySize.adjust(0.022), marginTop: "0.5%" }}>{postContent}</p>
                 </div>
               </div>
 
-              {/* Post content preview - limited to 3 lines */}
-              <div
-                className='preContext'
-                style={{
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  display: "-webkit-box",
-                  WebkitBoxOrient: "vertical",
-                  WebkitLineClamp: 3,
-                }}
-              >
-                <p style={{ fontSize: mySize.adjust(0.022), marginTop: "0.5%" }}>{postContent}</p>
-              </div>
+              {/* Read more button */}
+              <button className='forMore' onClick={() => goToPost(post._id)} style={{ fontSize: mySize.adjust(0.02), letterSpacing: "0.3em", ...(isVertical ? verticalMoreStyle : {}) }}>
+                More...
+              </button>
             </div>
-
-            {/* Read more button */}
-            <button className='forMore' onClick={() => goToPost(post._id)} style={{ fontSize: mySize.adjust(0.02), letterSpacing: "0.3em", ...(isVertical ? verticalMoreStyle : {}) }}>
-              More...
-            </button>
-          </div>
-        );
-      })}
+          );
+        })}
 
       {/* Pagination component */}
       {isVertical && (
--- my-app/src/Component/Utils/FormAccess.js
+++ my-app/src/Component/Utils/FormAccess.js
@@ -31,14 +31,14 @@ function FormAccess(props) {
 
   return (
     <div className='formAccess' id='formMask'>
-      <form className='formAccess' id='background' onSubmit={redirection}>
+      <form className='formAccess' id='background' onSubmit={redirection} style={{ width: isVertical ? "80vw" : "" }}>
         <button type='button' className='formAccess' id='closeBtn' onClick={() => props.setAccess(false)}></button>
         <h1 className='formAccess' style={{ fontSize: mySize.adjust(0.04) }}>
           Your School:
         </h1>
         <div style={{ display: "flex", width: "100%", height: "25%", justifyContent: "center", alignItems: "center" }}>
           <SchoolSelection cl='formAccess' setSchool={setSchool} />
-          <button type='submit' className='formAccess' id='submitBtn' style={{ fontSize: mySize.adjust(0.045) }}>
+          <button type='submit' className='formAccess' id='submitBtn' style={{ fontSize: isVertical ? mySize.adjust(0.08) : mySize.adjust(0.045) }}>
             ➭
           </button>
         </div>
